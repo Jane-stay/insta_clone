cd98d089f387d0c188acf77256b9f133a2118d76
SoHyun <staywithjanejane@gmail.com>
2024-12-18 23:32:07 +0900

    4

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
 let modalButton = document.getElementById("modal-setting");
 let closeButton = document.getElementById("close");
 let modal = document.getElementById("modal");
-
-
+let body = document.querySelector("body");
+let close = document.querySelector(".close");
 
 modalButton.addEventListener("click", ()=>{
 modal.classList.add("visible");
+body.classList.add("darkback");
+close.classList.add("visible");
 }
 )
 
 closeButton.addEventListener("click", ()=>{
 modal.classList.remove("visible");
-// modal.classList.add("invisible");
+body.classList.remove("darkback");
+close.classList.remove("visible");
 })
